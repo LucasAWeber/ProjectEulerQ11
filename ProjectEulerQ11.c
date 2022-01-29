b7107e25ca17a14e0540a4242a8b4301528e3b36
Lucas Weber <lweber07@uoguelph.ca>
2022-01-28 20:53:43 -0500

    determines products of up, down, left, right, or diagonally

--- ProjectEulerQ11.c
+++ ProjectEulerQ11.c
@@ -1,24 +1,82 @@
 #include <stdio.h>
 #define ADJACENT 4
+#define LEN 20
 
 int main(void)
 {
     FILE * fptr = fopen("test.txt", "r");
-    int num[20][20];
-    for (int i = 0; i < 20; i++)
+    int num[LEN][LEN];
+    int maxProduct = 0;
+    int curProduct = 0;
+
+    // reads to array
+    for (int i = 0; i < LEN; i++)
     {
-        for (int j = 0; j < 20; j++)
+        for (int j = 0; j < LEN; j++)
         {
             fscanf(fptr, "%d", &num[i][j]);
         }
     }
 
-    for (int i = 0; i < 20; i++)
+    // prints array
+    for (int i = 0; i < LEN; i++)
     {
-        for (int j = 0; j < 20; j++)
+        for (int j = 0; j < LEN; j++)
         {
             printf("%d ", num[i][j]);
         }
         printf("\n");
+    }
+
+    // reads product of 4 horizontal adjacent nums
+    for (int i = 0; i < LEN; i++)
+    {
+        for (int j = 0; j < LEN-3; j++)
+        {
+            curProduct = num[i][j] * num[i][j+1] * num[i][j+2] * num[i][j+3];
+            if (maxProduct < curProduct)
+            {
+                maxProduct = curProduct;
+            }
+        }
+    }
+
+    // reads product of 4 vertical adjacent nums
+    for (int i = 0; i < LEN; i++)
+    {
+        for (int j = 0; j < LEN-3; j++)
+        {
+            curProduct = num[j][i] * num[j+1][i] * num[j+2][i] * num[j+3][i];
+            if (maxProduct < curProduct)
+            {
+                maxProduct = curProduct;
+            }
+        }
+    }
+
+    // reads product of 4 diagonal left to right
+    for (int i = 0; i < LEN-3; i++)
+    {
+        for (int j = 0; j < LEN-3; j++)
+        {
+            curProduct = num[i][j] * num[i+1][j+1] * num[i+2][j+2] * num[i+3][j+3];
+            if (maxProduct < curProduct)
+            {
+                maxProduct = curProduct;
+            }
+        }
+    }
+
+    // reads product of 4 diagonal right to left
+    for (int i = 3; i < LEN; i++)
+    {
+        for (int j = 3; j < LEN; j++)
+        {
+            curProduct = num[i][j] * num[i+1][j-1] * num[i+2][j-2] * num[i+3][j-3];
+            if (maxProduct < curProduct)
+            {
+                maxProduct = curProduct;
+            }
+        }
     }
 }
